e16d3beb9b02008fc4dbad1d2de4f135d6cd5ad1
Sakthivel20028 <138582692+Sakthivel20028@users.noreply.github.com>
2026-07-13 13:20:59 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 5
+print(type(variable))
+
+variable = 5.6
+print(type(variable))
+
+variable = False
+print(type(variable))
+
+variable = "Sakthivel"
+print(type(variable))
+
+variable = [3,4,6,6]
+print(type(variable))
