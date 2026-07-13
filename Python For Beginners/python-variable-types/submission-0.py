9d195f7bc1d8f3162980b4ea3f42cd92590d3e85
Sakthivel20028 <138582692+Sakthivel20028@users.noreply.github.com>
2026-07-13 13:18:12 +0530

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,14 @@
+integer_type = 6
+float_type = 6.0
+bool_type = True
+string_type = "Sakthivel"
+list_type = [1,2,3,4,5]
+
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
