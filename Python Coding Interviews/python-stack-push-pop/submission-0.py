5b777f52ea53056868140711d2df2e1db44b13f1
Sakthivel20028 <138582692+Sakthivel20028@users.noreply.github.com>
2026-07-23 22:38:16 +0530

    Add: python-stack-push-pop - submission-0

---
+++ Python Coding Interviews/python-stack-push-pop/submission-0.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+def reverse_list(arr: List[int]) -> List[int]:
+    reversed_stack = []
+    for i in range(len(arr)):
+        element = arr.pop()
+        reversed_stack.append(element)
+    return reversed_stack    
+
+
+# do not modify below this line
+print(reverse_list([1, 2, 3]))
+print(reverse_list([3, 2, 1, 4, 6, 2]))
+print(reverse_list([1, 9, 7, 3, 2, 1, 4, 6, 2]))
