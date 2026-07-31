7aac7c9daafa5a18a8ae891a141cba1e6c9a96ec
Sakthivel20028 <138582692+Sakthivel20028@users.noreply.github.com>
2026-07-31 19:17:39 +0530

    Add: moving-average-from-data-stream - submission-0

---
+++ Data Structures & Algorithms/moving-average-from-data-stream/submission-0.py
@@ -0,0 +1,23 @@
+class MovingAverage:
+    
+
+    def __init__(self, size: int):
+        self.size = size
+        self.queue = deque()
+        self.total = 0
+
+    def next(self, val: int) -> float:
+        if len(self.queue) == self.size:
+            self.total -= self.queue.popleft()
+
+        self.queue.append(val)
+        self.total += val
+
+        return self.total / len(self.queue)   
+
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
